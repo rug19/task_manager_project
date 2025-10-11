ad6844e8de135fd6b01b9b92a515861d03501c62
rug19 <rugcosta1234@gmail.com>
2025-10-11 10:06:30 -0300

    feat: Setup initial project structure

---
+++ backend/src/test/java/com/task_manager_api/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task_manager_api.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Frontend is running...</h1>
+    </>
+  );
+}
+
+export default App;
