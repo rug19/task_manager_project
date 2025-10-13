a91cf6b12bd098823395f0751b1e441babfc4ca1
rug19 <rugcosta1234@gmail.com>
2025-10-13 13:07:12 -0300

    feat: Implement the edit activity method

--- frontend/src/components/activityCard.tsx
+++ frontend/src/components/activityCard.tsx
@@ -5,14 +5,17 @@ import IconButton from "./button";
 type ActivityCardProps = {
   activities: string[];
   onAddActivity: (activity: string) => void;
+  onUpdateActivity: (idx: number, newValue: string) => void;
 };
 
 export default function ActivityCard({
   activities,
   onAddActivity,
+  onUpdateActivity,
 }: ActivityCardProps) {
   const [creatingActivity, setCreatingActivity] = useState(false);
   const [activityInput, setActivityInput] = useState("");
+  const [editingIdx, setEditingIdx] = useState<number | null>(null);
 
   function handleAddActivity() {
     if (activityInput.trim()) {
@@ -21,44 +24,75 @@ export default function ActivityCard({
       setCreatingActivity(false);
     }
   }
+
+  function handleEditActivity(idx: number) {
+    setEditingIdx(idx);
+    setActivityInput(activities[idx]);
+  }
+
+  function handleUpdateActivity() {
+    if (activityInput.trim() && editingIdx !== null && onUpdateActivity) {
+      onUpdateActivity(editingIdx, activityInput);
+      setEditingIdx(null);
+      setActivityInput("");
+    }
+  }
+
   return (
     <div>
       {activities.map((activity, idx) => (
         <div
           key={idx}
-          className="bg-white border  p-2 mb-2 text-black font-semibold"
+          className="bg-white border  p-2 mb-2 text-black font-semibold cursor-pointer"
+          onClick={() => handleEditActivity(idx)}
         >
           {activity}
         </div>
       ))}
-      {creatingActivity ? (
-        <Modal
-          isOpen={creatingActivity}
-          onClose={() => setCreatingActivity(false)}
-        >
-          <textarea
-            placeholder="Descrição da atividade"
-            value={activityInput}
-            onChange={(e) => setActivityInput(e.target.value)}
-            className="border rounded px-2 py-1 w-full mb-4 mt-4  focus:outline-none"
-            rows={3}
-          />
-          <div className="flex justify-center gap-2">
-            <IconButton
-              label="Salvar"
-              className="bg-blue-600 text-white px-4 py-2 rounded  "
-              onClick={handleAddActivity}
-            />
-          </div>
-        </Modal>
-      ) : (
-        <IconButton
-          onlyIcon={false}
-          label="Novo Card +"
-          className="font-semibold text-blue-700 text-[18px]"
-          onClick={() => setCreatingActivity(true)}
+      {/* Modal to create an activity */}
+      <Modal
+        isOpen={creatingActivity}
+        onClose={() => setCreatingActivity(false)}
+      >
+        <textarea
+          placeholder="Descrição da atividade"
+          value={activityInput}
+          onChange={(e) => setActivityInput(e.target.value)}
+          className="border rounded px-2 py-1 w-full mb-4 mt-4  focus:outline-none"
+          rows={3}
         />
-      )}
+        <div className="flex justify-center gap-2">
+          <IconButton
+            label="Salvar"
+            className="bg-blue-600 text-white px-4 py-2 rounded  "
+            onClick={handleAddActivity}
+          />
+        </div>
+      </Modal>
+      {/* Modal to edit an activity */}
+      <Modal isOpen={editingIdx !== null} onClose={() => setEditingIdx(null)}>
+        <textarea
+          placeholder="Editar descrição da atividade"
+          value={activityInput}
+          onChange={(e) => setActivityInput(e.target.value)}
+          className="border rounded px-2 py-1 w-full mb-4 mt-4 focus:outline-none"
+          rows={3}
+        />
+        <div className="flex justify-center gap-2">
+          <IconButton
+            label="Salvar"
+            className="bg-blue-600 text-white px-4 py-2 rounded"
+            onClick={handleUpdateActivity}
+          />
+        </div>
+      </Modal>
+
+      <IconButton
+        onlyIcon={false}
+        label="Novo Card +"
+        className="font-semibold text-blue-700 text-[18px]"
+        onClick={() => setCreatingActivity(true)}
+      />
     </div>
   );
 }
